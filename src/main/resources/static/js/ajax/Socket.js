b76c3a33c20becb9047441cdadfa0af32d8a2af2
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2017-08-06 15:06:42 +0300

    desktop notification on task crash

--- src/main/resources/static/js/ajax/Socket.js
+++ src/main/resources/static/js/ajax/Socket.js
@@ -7,29 +7,19 @@ define([
   const socket = new SockJS('/socket');
   const stomp = Stomp.over(socket);
   stomp.connect({}, function (f) {
-    console.log('Connected');
     stomp.subscribe('/topic/task-instance', function (d) {
       const instance = JSON.parse(d.body);
-      console.log(instance);
       Push.create('Task Crash',{
-        silent: false,
-        body: `${instance.name} crashed`
+         body: `${instance.name} crashed`,
+         icon: 'img/octo-red-32.png'
       });
     });
   },function(e){
     console.log(e)
   });
   
-  
-  
-  
-  const connect = function() {
-
-  };
-  
-  
+    
   return {
-    connect,
   }
 
 
