06987695e3b82690cd283d255e657343a7805168
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2017-08-14 12:41:32 +0300

    task error deno from options

--- src/main/resources/static/js/ajax/Socket.js
+++ src/main/resources/static/js/ajax/Socket.js
@@ -1,20 +1,25 @@
 define([
 	'underscore',
   'plugins/Message',
-], function (_, Message) {
+  'ajax/User'
+], function (_, Message, User) {
 	'use strict';
    
   const socket = new SockJS('/socket');
   const stomp = Stomp.over(socket);
   stomp.connect({}, function (f) {
     stomp.subscribe('/topic/task-instance-error', function (d) {
-      const instance = JSON.parse(d.body);
-      Push.create('Task Crash',{
-         body: `${instance.name} crashed`,
-         icon: 'img/octo-red-32.png'
-      });
-      const audio = new Audio('sound/error.mp3');
-      audio.play();  
+      if(User.me.deno && User.me.deno.taskError){
+        const instance = JSON.parse(d.body);
+        Push.create('Task Crash',{
+          body: `${instance.name} crashed`,
+          icon: 'img/octo-red-32.png'
+        });
+        if(User.me.deno.sound){
+          const audio = new Audio('sound/error.mp3');
+          audio.play();
+        }
+      } 
     });
   },function(e){
     console.log(e)
