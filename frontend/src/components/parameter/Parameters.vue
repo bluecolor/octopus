8e75311a53a4a6ae2757508c533a4cb27e0cc82a
ceyhun kerti <ceyhun.kerti@gmail.com>
2018-02-17 11:52:04 +0300

    parameters

--- frontend/src/components/parameter/Parameters.vue
+++ frontend/src/components/parameter/Parameters.vue
@@ -1,44 +1,45 @@
 <template lang="pug">
-.col-md-8.col-md-offset-2(v-if="collection.length > 0 || filter.length > 0")
-  .box.box-primary(style="border-top=0px")
-    .box-header.with-border
-      h3.box-title {{title}}
-      .box-tools.pull-right
-        .has-feedback.table-search
-          input.form-control.input-sm.search-box(autofocus=true, v-model="filter" type='text', placeholder='Search')
-    .box-body.no-padding
-      .table-controls
-        a.btn.btn-default.btn-sm(@click='findAll' type='button', data-toggle="tooltip" title="Reload",)
-          i.fa.fa-refresh.text-blue.fa-lg
-        router-link.btn.btn-default.btn-sm(to='/parameter',data-toggle="tooltip" title="New",)
-          i.fa.fa-plus.text-green.fa-lg
-        router-link.btn.btn-default.btn-sm(to='/import' data-toggle="tooltip" title="Import")
-          i.fa.fa-upload.text-yellow.fa-lg
-        a.btn.btn-default.btn-sm(@click="remove(selected[0])", data-toggle="tooltip" title="Delete", :class="selected.length > 0 ? '':'hidden'")
-          i.fa.fa-trash-o.text-danger.fa-lg
-                
-      .table-responsive.connection-items
-        table.table.table-hover
-          tbody
-            tr(v-for="m in collection")
-              td(style="width:20px")
-                label.el-checkbox
-                  span.el-checkbox__input(:class="selected.indexOf(m.id)>-1 ? 'is-checked':''")
-                    span.el-checkbox__inner
-                    input.el-checkbox__original(type='checkbox', v-model="selected" :value ='m.id')
-              td 
-                router-link(:to="'parameter/' + m.id" ) {{m.name}}
+.row
+  .col-md-8.col-md-offset-2(v-if="collection.length > 0 || filter.length > 0")
+    .box.box-primary(style="border-top=0px")
+      .box-header.with-border
+        h3.box-title {{title}}
+        .box-tools.pull-right
+          .has-feedback.table-search
+            input.form-control.input-sm.search-box(autofocus=true, v-model="filter" type='text', placeholder='Search')
+      .box-body.no-padding
+        .table-controls
+          a.btn.btn-default.btn-sm(@click='findAll' type='button', data-toggle="tooltip" title="Reload",)
+            i.fa.fa-refresh.text-blue.fa-lg
+          router-link.btn.btn-default.btn-sm(to='/parameter',data-toggle="tooltip" title="New",)
+            i.fa.fa-plus.text-green.fa-lg
+          router-link.btn.btn-default.btn-sm(to='/import' data-toggle="tooltip" title="Import")
+            i.fa.fa-upload.text-yellow.fa-lg
+          a.btn.btn-default.btn-sm(@click="remove(selected[0])", data-toggle="tooltip" title="Delete", :class="selected.length > 0 ? '':'hidden'")
+            i.fa.fa-trash-o.text-danger.fa-lg
+                  
+        .table-responsive.connection-items
+          table.table.table-hover
+            tbody
+              tr(v-for="m in collection")
+                td(style="width:20px")
+                  label.el-checkbox
+                    span.el-checkbox__input(:class="selected.indexOf(m.id)>-1 ? 'is-checked':''")
+                      span.el-checkbox__inner
+                      input.el-checkbox__original(type='checkbox', v-model="selected" :value ='m.id')
+                td 
+                  router-link(:to="'parameter/' + m.id" ) {{m.name}}
 
-    .box-footer.clearfix
-      ul.pagination.pagination-sm.no-margin.pull-right  
-        uib-pagination(
-          :total-items="total" 
-          v-model="pagination" 
-          :max-size="maxPaginationSize" 
-          class="pagination-sm" 
-          :boundary-links="true" 
-          :rotate="false"
-        )  
+      .box-footer.clearfix
+        ul.pagination.pagination-sm.no-margin.pull-right  
+          uib-pagination(
+            :total-items="total" 
+            v-model="pagination" 
+            :max-size="maxPaginationSize" 
+            class="pagination-sm" 
+            :boundary-links="true" 
+            :rotate="false"
+          )  
   .align-center(v-if="collection.length === 0 && filter.length === 0")
     div.no-connection(style="width:330px; display: table-cell;vertical-align: middle;text-align: center;")
       div(style="width:100%; display: inline-block;")
